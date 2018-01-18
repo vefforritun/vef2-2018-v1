1e50abefc24a76a1a762cc1ec376985bb38f7202
Ólafur Sverrir Kjartansson <kjarni@gmail.com>
2018-01-18 00:46:00 +0000

    Bæta við verkefnalýsingu á verkefni 1

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
