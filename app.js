6a720246d9e780a41a0d43a287797f759e47b376
Ólafur Sverrir Kjartansson <kjarni@gmail.com>
2018-01-23 22:19:44 +0000

    Bæta við .eslintrc.js skrá og leyfa console.info, warn og erro

--- app.js
+++ app.js
@@ -6,5 +6,5 @@ const hostname = '127.0.0.1';
 const port = 3000;
 
 app.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+  console.info(`Server running at http://${hostname}:${port}/`);
 });
